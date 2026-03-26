fdcae9afb4e1ad1cf527cb94717d146a225cc6dd
procode-dev <shashikant.kumar@himotechglobal.com>
2026-03-26 11:47:55 +0530

    fix

--- app/firebase.ts
+++ app/firebase.ts
@@ -2,11 +2,14 @@ import { initializeApp, getApps } from "firebase/app";
 import { getMessaging, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyCpSRLYo7-mzewCngme8MOahc64XmLbD58",
   authDomain: "fir-2b576.firebaseapp.com",
   projectId: "fir-2b576",
+  storageBucket: "fir-2b576.firebasestorage.app",
   messagingSenderId: "318608703998",
   appId: "1:318608703998:web:4ffc4646aaeb4b0fcd40af",
+  measurementId: "G-NMJY9WEYEJ",
+  
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
